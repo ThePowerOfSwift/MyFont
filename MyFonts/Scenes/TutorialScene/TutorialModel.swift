c0c7ef99079865e05cd2357eb86256bdd0def1cb
Irakli Shelia <i.shelia@bog.ge>
2020-01-29 21:39:08 +0400

    added fonts

---
+++ MyFonts/Scenes/TutorialScene/TutorialModel.swift
@@ -0,0 +1,9 @@
+//
+//  TutorialModel.swift
+//  MyFonts
+//
+//  Created by Irakli on 1/29/20.
+//  Copyright © 2020 Irakli. All rights reserved.
+//
+
+import Foundation
---
+++ MyFonts/Scenes/TutorialScene/TutorialViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TutorialViewController.swift
+//  MyFonts
+//
+//  Created by Irakli on 1/29/20.
+//  Copyright © 2020 Irakli. All rights reserved.
+//
+
+import Foundation
