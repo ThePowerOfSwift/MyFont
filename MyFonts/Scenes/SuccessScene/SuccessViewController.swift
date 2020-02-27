e2938f8acd97763f26f1ceef2d451515471205a6
Irakli Shelia <i.shelia@bog.ge>
2020-02-28 02:27:10 +0400

    localized and refactored settings vc

--- MyFonts/Scenes/SuccessScene/SuccessViewController.swift
+++ MyFonts/Scenes/SuccessScene/SuccessViewController.swift
@@ -16,39 +16,46 @@ class SuccessViewController: UIViewController {
             settingsButton.layer.cornerRadius = settingsButton.frame.height/2
             settingsButton.clipsToBounds = true
             settingsButton.backgroundColor = THEME_MAIN_COLOR
+            settingsButton.setTitle(NSLocalizedString("settings.button.configure", comment: ""), for: .normal)
         }
     }
     
     @IBOutlet var titleLabel: UILabel! {
         didSet {
             titleLabel.text = NSLocalizedString("settings.title", comment: "Almost Set!")
+            titleLabel.textColor = LABEL_MAIN_COLOR
         }
     }
     @IBOutlet var tutorialLabel1:UILabel! {
         didSet {
             tutorialLabel1.text = NSLocalizedString("settings.tutorial.step1", comment: "")
+            tutorialLabel1.textColor = LABEL_MAIN_COLOR
         }
     }
     @IBOutlet var tutorialLabel2:UILabel! {
         didSet {
             tutorialLabel2.text = NSLocalizedString("settings.tutorial.step2", comment: "")
+            tutorialLabel2.textColor = LABEL_MAIN_COLOR
         }
     }
     @IBOutlet var tutorialLabel3:UILabel! {
         didSet {
             tutorialLabel3.text = NSLocalizedString("settings.tutorial.step3", comment: "")
+            tutorialLabel3.textColor = LABEL_MAIN_COLOR
         }
     }
     
     @IBOutlet var tutorialLabel4:UILabel! {
         didSet {
             tutorialLabel4.text = NSLocalizedString("settings.tutorial.step4", comment: "")
+            tutorialLabel4.textColor = LABEL_MAIN_COLOR
         }
     }
     
     @IBOutlet var tutorialLabel5:UILabel! {
         didSet {
             tutorialLabel5.text = NSLocalizedString("settings.tutorial.step5", comment: "")
+            tutorialLabel5.textColor = LABEL_MAIN_COLOR
         }
     }
     
